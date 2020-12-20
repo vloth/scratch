89dbf87b590c4544f485dc1da5a8abfcc979d4d5
Francisco Berrocal <fran.fhb@gmail.com>
2020-12-20 11:49:06 +0100

    Σ feat: Add initial commit

---
+++ cird/project.clj
@@ -0,0 +1,6 @@
+(defproject cird "0.1.0-SNAPSHOT"
+  :description "random generative art made with clojure and quill - port from bird"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [quil "3.1.0"]])
